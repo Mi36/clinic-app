ebee258a57e415d402784fc682cf84b45f5febe2
admin <admin@admins-iMac.local>
2020-08-20 16:56:27 +0530

    client answer screen design updated.

--- src/screens/clientAnswersScreen.js
+++ src/screens/clientAnswersScreen.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {clientAnswerFetch} from '../actions/questionAction';
 
@@ -16,10 +16,45 @@ export default function clientAnswersScreen({navigation}) {
       data={answers}
       keyExtractor={(item) => item.question}
       renderItem={({item}) => (
-        <Text>
-          {item.question}: {item.answer}
-        </Text>
+        <View style={styles.placeItem}>
+          <View style={styles.infoContainer}>
+            <Text style={styles.title}>Q: {item.question}</Text>
+            <Text>A: {item.answer}</Text>
+          </View>
+        </View>
       )}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  placeItem: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    paddingVertical: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 70,
+    height: 70,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  infoContainer: {
+    marginLeft: 25,
+    width: 250,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  title: {
+    color: 'black',
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  address: {
+    color: '#666',
+    fontSize: 16,
+  },
+});

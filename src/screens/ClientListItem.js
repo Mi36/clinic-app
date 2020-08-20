df3791b5e288c70ee4467290adc0d348bc8d833d
admin <admin@admins-iMac.local>
2020-08-20 16:56:52 +0530

    client list design updated

--- src/screens/ClientListItem.js
+++ src/screens/ClientListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function ClientListItem(props) {
   return (
@@ -9,9 +9,45 @@ export default function ClientListItem(props) {
           itemId: props.data.id,
         });
       }}>
-      <View>
-        <Text>{props.data.id}</Text>
+      <View style={styles.placeItem}>
+        <View style={styles.infoContainer}>
+          <Text style={styles.title}>{props.data.id}</Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  placeItem: {
+    borderColor: '#ccc',
+    borderTopColor: '#ccc',
+    borderBottomWidth: 1,
+    borderWidth: 1,
+    paddingVertical: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 70,
+    height: 70,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  infoContainer: {
+    marginLeft: 25,
+    width: 250,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  title: {
+    color: 'black',
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  address: {
+    color: '#666',
+    fontSize: 16,
+  },
+});

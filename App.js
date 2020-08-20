8d3ad3f75fc94038054c3f161df4f48d08b80a05
admin <admin@admins-iMac.local>
2020-08-20 15:18:08 +0530

    Added redux persist.

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
+import AsyncStorage from '@react-native-community/async-storage';
 import {ApplicationProvider} from '@ui-kitten/components';
 import AppNavigator from './src/navigation/AppNavigator';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import ReduxThunk from 'redux-thunk';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore, persistReducer} from 'redux-persist';
 import reducers from './src/reducers';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['questions', 'clientQuestions'],
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = createStore(persistedReducer, {}, applyMiddleware(ReduxThunk));
+let persistor = persistStore(store);
+
 export default App = () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
-      <AppNavigator />
-    </Provider>
-  </ApplicationProvider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <AppNavigator />
+      </ApplicationProvider>
+    </PersistGate>
+  </Provider>
 );
